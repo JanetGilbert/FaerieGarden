f69299e5bb51787310abeb314e95751957d310b6
JanetGilbert <janet@wadjeteyegames.com>
2021-05-24 21:06:45 -0400

    Added simple camera script and player animations.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 
 // Simple camera follow script
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
 
     [SerializeField] private Transform player; // Drag player here
+
     private Vector3 cameraOffset;
+    private Camera thisCamera;
+    private Quaternion cameraRotateStart;
 
     void Start()
     {
-        cameraOffset = Camera.main.transform.position - player.transform.position;
+        thisCamera = GetComponent<Camera>();
+        cameraOffset = thisCamera.transform.position - player.transform.position;
+        cameraRotateStart = thisCamera.transform.rotation;
     }
 
     // Always do camera follow code last, after player has moved.
     void LateUpdate()
     {
-        Camera.main.transform.position = player.transform.position + cameraOffset;
+        thisCamera.transform.position = (player.transform.forward * cameraOffset.z) +
+                                        new Vector3(player.transform.position.x, 
+                                        player.transform.position.y + cameraOffset.y, 
+                                        player.transform.position.z);
+                                                                    
+        thisCamera.transform.rotation = player.transform.rotation *  cameraRotateStart;
     }
 }
--- Assets/Scripts/CharacterControllerMove.cs
+++ Assets/Scripts/CharacterControllerMove.cs
@@ -1,76 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-
+/// <summary>
+/// Implements player character control system and animation.
+/// </summary>
 public class CharacterControllerMove : MonoBehaviour
 {
-    CharacterController characterController;
+    [Header("Speed Settings")]
+    [SerializeField] private float speed = 6.0f;
+    [SerializeField] private float jumpSpeed = 8.0f;
+    [SerializeField] private float gravity = 20.0f;
+    [SerializeField] private float rotateSpeed = 10.0f; 
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    // Controls
+    private float turnMove;
+    private float forwardMove;
+    private bool jump;
 
-    private Vector3 moveDirection = Vector3.zero;
+    // Cache
+    private CharacterController characterController;
+    private Animator animator;
 
-    public Animator animator;
-
-    private float xMove;
-    private float yMove;
-    bool jump;
+    //Movement
+    private float jumpY;
+    Vector3 moveDirection = Vector3.zero;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-       
-
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+        
+
         animator.SetBool("Grounded", characterController.isGrounded);
 
-        if (characterController.isGrounded)
+        if (characterController.isGrounded) // We are grounded, so recalculate move direction
         {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-            moveDirection = new Vector3(xMove, 0.0f, yMove);
-            moveDirection *= speed;
+            transform.Rotate(0.0f, turnMove * rotateSpeed * Time.fixedDeltaTime, 0.0f);
 
-            // Face in direction of movement.
-            if (moveDirection.magnitude > float.Epsilon)
-            {
-                transform.rotation = Quaternion.LookRotation(moveDirection);
-            }
+            moveDirection = transform.forward * forwardMove  * speed;
 
-            animator.SetFloat("MoveSpeed", moveDirection.magnitude);
+            animator.SetFloat("MoveSpeed", forwardMove);
+            animator.SetFloat("TurnSpeed", Mathf.Abs(turnMove));
 
             if (jump)
             {
-                moveDirection.y = jumpSpeed;
+                jumpY = jumpSpeed;
             }
-
-
-
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
         // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.fixedDeltaTime;
+        jumpY -= gravity * Time.fixedDeltaTime;
+
+        moveDirection = new Vector3(moveDirection.x, jumpY, moveDirection.z);
 
         // Move the controller
         characterController.Move(moveDirection * Time.fixedDeltaTime);
-
-
-        
     }
 
+    // Control input
     void Update()
     {
-        xMove = Input.GetAxis("Horizontal");
-        yMove = Input.GetAxis("Vertical");
-
+        turnMove = Input.GetAxis("Horizontal");
+        forwardMove = Input.GetAxis("Vertical");
         jump = Input.GetButton("Jump");
     }
 
